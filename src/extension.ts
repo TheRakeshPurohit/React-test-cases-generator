39e44b31bf315a35c916a896090faa1778f073e0
therakeshpurohit <rakeshpurohit909@gmail.com>
2020-12-25 09:55:44 +0530

    message when no workspace found

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "VSCode-TRP-RTCG" is now active!');
-	vscode.window.showInformationMessage('Congratulations, your extension "VSCode-TRP-RTCG" is now active!');
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -21,6 +19,22 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	let generateTests = vscode.commands.registerCommand('VSCode-TRP-RTCG.generateTests', () => {
+		// The code you place here will be executed every time your command is executed
+
+		if (vscode.workspace.name) {
+			// Display a message box to the user
+			vscode.window.showInformationMessage('Scanning src/components');
+			let a = vscode.workspace.findFiles("/src/components/*.js");
+			vscode.window.showInformationMessage(a.toString());
+			vscode.window.showInformationMessage('Generating Tests for you !');
+		} else {
+			vscode.window.showInformationMessage("No Workspace Found !");
+		}
+	});
+
+	context.subscriptions.push(generateTests);
 }
 
 // this method is called when your extension is deactivated
